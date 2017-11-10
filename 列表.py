a1ae10ed723a2b6ad79b35ff55ed25b6f6b85fc8
Museday <33245362+Museday@users.noreply.github.com>
2017-11-10 13:47:16 +0800

    Add files via upload

---
+++ 列表.py
@@ -0,0 +1,18 @@
+Python 2.7.13 (v2.7.13:a06454b1afa1, Dec 17 2016, 20:42:59) [MSC v.1500 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> L = ['Google', 'Runoob', 'Taobao']
+>>> L[2]
+'Taobao'
+>>> L[-2]
+'Runoob'
+>>> L[1:]
+['Runoob', 'Taobao']
+>>> 
+>>> 
+>>> list_2d = [ [0 for i in range(5)] for i in range(5)]
+>>> list_2d[0].append(3)
+>>> list_2d[0].append(5)
+>>> list_2d[2].append(7)
+>>> list_2d
+[[0, 0, 0, 0, 0, 3, 5], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 7], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0]]
+>>> 
